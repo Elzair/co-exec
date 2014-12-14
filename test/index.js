14c70ae7f6217c53dd833ee9ee628b8e4313f444
Philip Woods <elzairthesorcerer@gmail.com>
2014-12-14 11:41:18 -0500

    Replaced Makefile with npm test script and added co-mocha

--- test/index.js
+++ test/index.js
@@ -1,35 +1,27 @@
 
 var co = require('co');
+var co_mocha = require('co-mocha');
 var exec = require('..');
-var assert = require('assert');
+var should = require('chai').should();
 
 describe('exec(cmd)', function(){
-  it('should return stdout', function(done){
-    co(function *(){
-      var ret = yield exec('echo hello');
-      ret.should.equal('hello\n');
-      done();
-    });
+  it('should return stdout', function*(){
+    var ret = yield exec('echo hello');
+    ret.should.equal('hello\n');
   })
 
-  it('should throw on error', function(done){
-    co(function *(){
-      try {
-        var ret = yield exec('does_not_exist');
-      } catch (err) {
-        err.message.should.include('does_not_exist');
-        done();
-      }
-    });
+  it('should throw on error', function*(){
+    try {
+      var ret = yield exec('does_not_exist');
+    } catch (err) {
+      err.message.should.include('does_not_exist');
+    }
   })
 })
 
 describe('exec(cmd, opts)', function(){
-  it('should set options', function(done){
-    co(function *(){
-      var ret = yield exec('echo hello', { encoding: 'base64' });
-      new Buffer(ret, 'base64').toString().should.equal('hello\n');
-      done();
-    });
+  it('should set options', function*(){
+    var ret = yield exec('echo hello', { encoding: 'base64' });
+    new Buffer(ret, 'base64').toString().should.equal('hello\n');
   })
 })
